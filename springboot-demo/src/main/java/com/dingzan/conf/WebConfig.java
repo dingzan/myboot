b1b80bc99edaa8d9289cd07dc250ad4cdc03dd01
dingzan <1151798326@qq.com>
2018-09-14 15:10:29 +0800

    设置date类型转化为json方式

--- springboot-demo/src/main/java/com/dingzan/conf/WebConfig.java
+++ springboot-demo/src/main/java/com/dingzan/conf/WebConfig.java
@@ -1,9 +1,18 @@
 package com.dingzan.conf;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableWebMvc
@@ -14,4 +23,21 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         //允许全部请求跨域
         registry.addMapping("/**");
     }
+    
+    //定义时间格式转换器
+    @Bean
+    public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        converter.setObjectMapper(mapper); return converter;
+    }
+    //添加转换器
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jackson2HttpMessageConverter());
+    }
+    
+    
 }
