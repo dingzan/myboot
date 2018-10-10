c166d26717685834996dda1f618d36213862c6a3
dingzan <1151798326@qq.com>
2018-10-10 10:47:13 +0800

    整合mq

--- springboot-ActivityMQ-example/src/main/java/com/dingzan/conf/JmsConfiguration.java
+++ springboot-ActivityMQ-example/src/main/java/com/dingzan/conf/JmsConfiguration.java
@@ -1,14 +1,11 @@
 package com.dingzan.conf;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.connection.CachingConnectionFactory;
-import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.ConnectionFactory;
 
